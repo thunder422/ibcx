5e67be85032a7f69a1222e3e81a6509498c67a40
Thunder422 <thunder422@verizon.net>
2016-05-10 20:47:56 -0400

    initial-setup: final cleanup
    reformatted cpp11.cpp to new coding style (expand tabs, i.e. no tab
    characters, K&R braces) and added copyright/license comments header
    
    renamed project from IBCX to just IBC; modify coverage changed to just
    produce warning message if not debug build type (to be easier to use
    with QtCreator); simplied activate message
    
    added *.user to .gitignore (for QtCreator)

--- test/cpp11.cpp
+++ test/cpp11.cpp
@@ -1,3 +1,10 @@
+/* vim:ts=4:sw=4:et:sts=4:
+ *
+ * Copyright 2016 Thunder422.  All rights reserved.
+ * Distributed under GNU General Public License Version 3
+ * (See accompanying file LICENSE or <http://www.gnu.org/licenses/>)
+ */
+
 #include <iostream>
 
 // test enum class
@@ -5,25 +12,24 @@ enum class Test {First, Second, Third};
 
 int main()
 {
-	// test initializers
-	int i {1};
-	double d {2.0};
+    // test initializers
+    int i {1};
+    double d {2.0};
 
-	// test initializer list
-	int a[] {1, 2, 3, 4};
+    // test initializer list
+    int a[] {1, 2, 3, 4};
 
-	// test nullptr
-	int *pi {nullptr};
+    // test nullptr
+    int *pi {nullptr};
 
-	pi = &i;
+    pi = &i;
 
-	std::cout << d << a[1] << *pi << std::endl;
+    std::cout << d << a[1] << *pi << std::endl;
 
-	// test enum class
-	Test e {Test::Second};
+    // test enum class
+    Test e {Test::Second};
 
-	if (e != Test::Third)
-	{
-		std::cout << "enum class works\n";
-	}
+    if (e != Test::Third) {
+        std::cout << "enum class works\n";
+    }
 }
