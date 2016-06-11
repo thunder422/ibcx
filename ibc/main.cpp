9ce2a0901bbb3477572452b3db1b52f1bab25626
Thunder422 <thunder422@verizon.net>
2016-06-11 08:38:10 -0400

    ibc: started implementing standalone command line compiler
    TEST: build program and verify usage message when no arguments.
    
    A testing infrastructure needed to be implemented to run a program and
    test its output.  This was accomplished using a CMake script that runs
    the test.  This script first runs the program with the command line
    arguments to test and collects the result of the program into a variable
    and sends the output and error streams to an output file.  It checks
    that the result matches the expected result, and compares (using the
    built-in CMake compare files command) the output file with an expected
    output file.
    
    The new command line program was put into its own directory and CMake
    build file.  It is linked with the library.  Because the name of this
    command is the same as the library name (ultimately the library will be
    prefixed with 'lib', but CMake sees these as having the same target
    name), the target name was given a '-bin' suffix, and given its
    desired executable by setting its output name property.  The test files
    were put into a test sub-directory.
    
    The desired usage message was put into an expected usage file in the
    test directory.  To make the test pass, the main function simply outputs
    the usage message and returns a 1 code (failure).
    output

---
+++ ibc/main.cpp
@@ -0,0 +1,15 @@
+/* vim:ts=4:sw=4:et:sts=4:
+ *
+ * Copyright 2016 Thunder422.  All rights reserved.
+ * Distributed under GNU General Public License Version 3
+ * (See accompanying file LICENSE or <http://www.gnu.org/licenses/>)
+ */
+
+#include <iostream>
+
+
+int main()
+{
+    std::cerr << "usage: ibcx [-r] <source-file>" << std::endl;
+    return 1;
+}
---
+++ ibc/test/runTest.cmake
@@ -0,0 +1,26 @@
+# vim:ts=4:sw=4:et:sts=4:
+#
+# Copyright 2016 Thunder422.  All rights reserved.
+# Distributed under GNU General Public License Version 3
+# (See accompanying file LICENSE or <http://www.gnu.org/licenses/>)
+
+set(out_file ${TEST_NAME}.out)
+set(exp_file ${TEST_NAME}.exp)
+
+execute_process(COMMAND ${TEST_PROGRAM} ${TEST_ARGS}
+    RESULT_VARIABLE result
+    OUTPUT_FILE ${out_file}
+    ERROR_FILE ${out_file}
+)
+
+if (NOT ${result} STREQUAL ${TEST_EXPECT})
+    message(FATAL_ERROR "Test failed: ${TEST_PROGRAM} returned ${result}, expected ${TEST_EXPECT}")
+endif ()
+
+execute_process(COMMAND ${CMAKE_COMMAND} -E compare_files
+    ${out_file} ${SOURCE_DIR}/${exp_file}
+    RESULT_VARIABLE mismatch
+)
+if (mismatch)
+    message(FATAL_ERROR "Test failed: '${out_file}' doesn't match '${exp_file}'")
+endif ()
