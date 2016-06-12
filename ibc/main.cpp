0649ea825ae747d80c27c6efaac50ceb12b10f51
Thunder422 <thunder422@verizon.net>
2016-06-12 13:08:40 -0400

    ibc: cleaned up and refactored the code.
    The ibc main function was becoming large and unrulely, so it was
    refactored into two classes and many smaller function.  The first class
    is for processing the command lines arguments, and the second class is
    for compiling, recreating and executing the program.  For now all the
    code remains in the single source file.

--- ibc/main.cpp
+++ ibc/main.cpp
@@ -13,56 +13,170 @@
 #include "programunit.h"
 
 
-void usage();
+struct IbcError { };
+
+
+class IbcArguments {
+public:
+    IbcArguments(int argc, char *argv[]);
+    const std::string &getFileName() const;
+    bool getAlsoRecreate() const;
+
+private:
+    void checkNoArguments() const;
+    void checkExtraArguments() const;
+    void parseFileName();
+    bool parseAlsoRecreate();
+    void checkAlsoRecreate();
+    void error(const char *message, const std::string &argument) const;
+    void usage() const;
+
+    std::vector<std::string> args;
+    std::string file_name;
+    bool also_recreate;
+};
+
+
+class IbcProgram {
+public:
+    IbcProgram(const IbcArguments &arguments);
+    void compile();
+    void recreate();
+    void execute();
+
+private:
+    void checkFileOpen();
+
+    std::string file_name;
+    bool also_recreate;
+    std::ifstream ifs;
+    ProgramUnit program;
+};
+
 
 int main(int argc, char *argv[])
+try
 {
-    std::vector<std::string> args {argv, argv + argc};
+    IbcArguments arguments {argc, argv};
 
+    IbcProgram program {arguments};
+
+    program.compile();
+    program.recreate();
+    program.execute();
+}
+catch (const IbcError &) {
+    return 1;
+}
+
+// ----------------------------------------
+
+IbcArguments::IbcArguments(int argc, char *argv[]) :
+    args {argv, argv + argc}
+{
+    checkNoArguments();
+    checkExtraArguments();
+    parseFileName();
+    also_recreate = parseAlsoRecreate();
+}
+
+const std::string &IbcArguments::getFileName() const
+{
+    return file_name;
+}
+
+bool IbcArguments::getAlsoRecreate() const
+{
+    return also_recreate;
+}
+
+void IbcArguments::checkNoArguments() const
+{
     if (args.size() == 1) {
         usage();
-        return 1;
+        throw IbcError {};
     }
+}
+
+void IbcArguments::checkExtraArguments() const
+{
     if (args.size() > 3) {
-        std::cerr << "ibc: extra operand '" << args[3] << "'" << std::endl;
+        error("extra operand", args[3]);
         usage();
-        return 1;
+        throw IbcError {};
     }
+}
 
-    std::string file_name = args.size() == 2 ? args[1] : args[2];
-    bool also_recreate;
+void IbcArguments::parseFileName()
+{
+    file_name = args.size() == 2 ? args[1] : args[2];
+}
+
+bool IbcArguments::parseAlsoRecreate()
+{
     if (args.size() == 3) {
-        if (args[1] != "-r") {
-            std::cerr << "ibc: invalid option -- '" << args[1] << "'" << std::endl;
-            usage();
-            return 1;
-        }
-        also_recreate = true;
+        checkAlsoRecreate();
+        return true;
     } else {
-        also_recreate = false;
+        return false;
     }
+}
 
-    std::ifstream ifs(file_name);
+void IbcArguments::checkAlsoRecreate()
+{
+    if (args[1] != "-r") {
+        error("invalid option --", args[1]);
+        usage();
+        throw IbcError {};
+    }
+}
+
+void IbcArguments::error(const char *message, const std::string &argument) const
+{
+    std::cerr << "ibc: " << message << " '" << argument << "'" << std::endl;
+}
+
+void IbcArguments::usage() const
+{
+    std::cerr << "usage: ibc [-r] <source-file>" << std::endl;
+}
+
+// ----------------------------------------
+
+IbcProgram::IbcProgram(const IbcArguments &arguments) :
+    file_name {arguments.getFileName()},
+    also_recreate {arguments.getAlsoRecreate()},
+    ifs {file_name}
+{
+    checkFileOpen();
+}
+
+void IbcProgram::checkFileOpen()
+{
     if (!ifs.is_open()) {
-        std::cerr << "ibc: " << args[1] << ": could not open file" << std::endl;
-        return 1;
+        std::cerr << "ibc: " << file_name << ": could not open file" << std::endl;
+        throw IbcError {};
     }
+}
 
-    ProgramUnit program;
-
+void IbcProgram::compile()
+{
     if (!program.compileSource(ifs, std::cerr)) {
-        std::cerr << args[1] << ": contains errros, program not run" << std::endl;
-        return 1;
+        std::cerr << file_name << ": contains errros, program not run" << std::endl;
+        throw IbcError {};
     }
+}
+
+void IbcProgram::recreate()
+{
     if (also_recreate) {
         std::cout << "Program:" << std::endl;
         program.recreate(std::cout);
         std::cout << std::endl << "Executing..." << std::endl;
     }
-    program.run(std::cout);
 }
 
-void usage()
+void IbcProgram::execute()
 {
-    std::cerr << "usage: ibc [-r] <source-file>" << std::endl;
+    program.run(std::cout);
 }
