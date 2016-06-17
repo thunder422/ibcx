821619e6cd9ceb5bb87956c0d731b8688ca85987
Thunder422 <thunder422@verizon.net>
2016-06-17 19:14:30 -0400

    unit-tests: separated negate and exponential unit tests
    The expression unit tests was starting to grow and would become massive
    once all the operator unit tests are implemented.  To prevent this, the
    negate and exponential unit tests were separated into their own unit
    test source files.
    
    The negate unit tests were separated further into three separate test
    cases (compile, recreate and execute).  The exponential unit tests will
    follow the same pattern (but only the compile unit tests have been
    implemented so far).
    
    The negate unit tests were cleaned up a bit because they contained extra
    unnecessary tests (specifically those assertions that were checking if
    the numerical constants were compiled correctly, but this is already
    covered in the constant number unit tests - not necessary to repeat).
    Verification that the correct codes are actually there are again covered
    indirectly in the negate recreate and execute unit tests.
    
    The expression unit tests will be reserved for more general expression
    testing, for example, precedence of operators and parentheses handling.

---
+++ test/exponential_unittests.cpp
@@ -0,0 +1,73 @@
+/* vim:ts=4:sw=4:et:sts=4:
+ *
+ * Copyright 2016 Thunder422.  All rights reserved.
+ * Distributed under GNU General Public License Version 3
+ * (See accompanying file LICENSE or <http://www.gnu.org/licenses/>)
+ */
+
+#include "catch.hpp"
+#include "compileerror.h"
+#include "compiler.h"
+#include "programunit.h"
+
+
+TEST_CASE("compile exponential operator expressions", "[compile]")
+{
+    ProgramUnit program;
+
+    SECTION("make sure both operands and operator are parsed")
+    {
+        Compiler compiler {"3^2", program};
+        compiler.compileExpression(DataType::Null);
+
+        REQUIRE(compiler.peekNextChar() == EOF);
+    }
+    SECTION("make sure the operator is the correct character")
+    {
+        Compiler compiler {"3a2", program};
+        compiler.compileExpression(DataType::Null);
+
+        REQUIRE(compiler.peekNextChar() == 'a');
+    }
+    SECTION("make sure white space is allowed before the operator")
+    {
+        Compiler compiler {"3 ^2", program};
+        compiler.compileExpression(DataType::Null);
+
+        REQUIRE(compiler.peekNextChar() == EOF);
+    }
+    SECTION("make sure white space is allowed after the operator")
+    {
+        Compiler compiler {"3 ^ 2", program};
+        compiler.compileExpression(DataType::Null);
+
+        REQUIRE(compiler.peekNextChar() == EOF);
+    }
+    SECTION("make sure parsing stops if there is no operand")
+    {
+        Compiler compiler {"^2", program};
+        compiler.compileExpression(DataType::Null);
+
+        REQUIRE(compiler.peekNextChar() == '^');
+    }
+    SECTION("check for an error if there is a problem with the second operand")
+    {
+        Compiler compiler {"3^^", program};
+
+        SECTION("check that the error is thrown")
+        {
+            REQUIRE_THROWS_AS(compiler.compileExpression(DataType::Null), ExpNumExprError);
+        }
+        SECTION("check the message, column and length of the error thrown")
+        {
+            try {
+                compiler.compileExpression(DataType::Null);
+            }
+            catch (const ExpNumExprError &error) {
+                REQUIRE(error.column == 2);
+                REQUIRE(error.length == 1);
+            }
+        }
+    }
+}
+
